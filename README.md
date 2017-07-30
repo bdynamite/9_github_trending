02e0668ef8ced24e1cd17e94958490e65ab5c713
bdynamite <bdynamite@yandex.ru>
2017-07-30 12:47:51 +0300

    commit#1

--- README.md
+++ README.md
@@ -1,7 +1,43 @@
 # Github Trends
 
-[TODO. There will be project description]
+# How to Install
 
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+# Quickstart
+
+
+Example of script launch on Linux, Python 3.5:
+
+```#!bash
+
+$ python github_trending.py
+
+# output example
+********************
+Repository "trust"
+url: https://github.com/ncase/trust
+issues: 7
+Github→GitHub https://api.github.com/repos/ncase/trust/issues/14
+Grudger wins instead of the copycat https://api.github.com/repos/ncase/trust/issues/13
+...
+...
+Unable to clone the repo https://api.github.com/repos/ncase/trust/issues/12
+Cyrillic fonts https://api.github.com/repos/ncase/trust/issues/10
+********************
+Repository "open-source-flash"
+url: https://github.com/pakastin/open-source-flash
+issues: 16
+Flash spec is already open source right?  https://api.github.com/repos/pakastin/open-source-flash/issues/36
+...
+...
+
+
+```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,50 @@
+import requests
+import datetime
+
+TOP_SIZE = 3
+DAYS = 7
+REPO_URL = 'https://api.github.com/search/repositories'
+ISSUE_URL = 'https://api.github.com/repos/{}/{}/issues'
+
+
+def get_time():
+    n_days_ago = datetime.datetime.today() - datetime.timedelta(days=DAYS)
+    return n_days_ago.strftime('%Y-%m-%d')
 
 
 def get_trending_repositories(top_size):
-    pass
+    params = {
+        'q': 'created:>{}'.format(get_time()),
+        'sort': 'stars',
+        'order': 'desc',
+        'per_page': top_size
+    }
+    responce = requests.get(url=REPO_URL, params=params)
+    return [Repository(x) for x in responce.json()['items']]
+
+class Repository:
+
+    def __init__(self, params_dict):
+        self.url = params_dict['html_url']
+        self.name = params_dict['name']
+        self.owner = params_dict['owner']['login']
+        self.issues = get_open_issues_amount(self.owner, self.name)
+
+    def __repr__(self):
+        return 'Repository "{}"'.format(self.name)
+
+    def print_with_issues(self):
+        print('*'*20)
+        print(self)
+        print('url: {}'.format(self.url))
+        print('issues: {}'.format(len(self.issues)))
+        print('\n'.join(['{} {}'.format(x['title'], x['url']) for x in self.issues]))
 
 
 def get_open_issues_amount(repo_owner, repo_name):
-    pass
+    responce = requests.get(url=ISSUE_URL.format(repo_owner, repo_name))
+    return responce.json()
 
 if __name__ == '__main__':
-    pass
+    repositories = get_trending_repositories(TOP_SIZE)
+    [x.print_with_issues() for x in sorted(repositories, key= lambda x: len(x.issues))]
